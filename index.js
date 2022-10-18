87dc551bfac9ddd61700086e0d566bbd407c1b97
mohamedEl-nbery <58948482+mohamedEl-nbery@users.noreply.github.com>
2022-10-18 11:38:57 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const btnEl = document.querySelector(".btn");
+const containerEl = document.querySelector(".container");
+const popupContainerEl = document.querySelector(".popup-container");
+const closeIconEl = document.querySelector(".close-icon");
+
+
+btnEl.addEventListener("click", ()=>{
+    containerEl.classList.add("active");
+    popupContainerEl.classList.remove("active");
+    
+
+});
+
+closeIconEl.addEventListener("click", ()=>{
+    containerEl.classList.remove("active");
+    popupContainerEl.classList.add("active");
+});
